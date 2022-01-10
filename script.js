c705a4ee407181a8343255305289e1a49460a070
Daniel Z <75264908+Ravost99@users.noreply.github.com>
2022-01-10 15:34:41 +0000

    Scripts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-if (window.location.href == "https://ravost99githubio.ravost.repl.co") {
-  window.location.href = "https://ravost99.github.io"
+if (window.location.href == "https://ravost99githubio.ravost.repl.co/") {
+  window.location.href = "https://ravost99.github.io/"
+} else if (window.location.href == "https://4e63e46b-2a8b-4a1c-b603-a937b5c005e1.id.repl.co/") {
+  window.location.href = "https://ravost99.github.io/"
 }
